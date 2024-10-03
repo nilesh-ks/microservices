5c3c622997975683a6d89e1934452f927990b386
Nilesh Sinha <nilesh.sinha1731@gmail.com>
2024-10-04 02:23:28 +0530

    Implemented exception and input data validations for accounts microservice

--- section2/accounts/src/main/java/com/project/accounts/dto/CustomerDto.java
+++ section2/accounts/src/main/java/com/project/accounts/dto/CustomerDto.java
@@ -1,15 +1,24 @@
 package com.project.accounts.dto;
 
 import com.project.accounts.entity.Accounts;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
 import lombok.Data;
 
 @Data
 public class CustomerDto {
 
+    @NotEmpty(message = "Name cannot be null or empty")
+    @Size(min = 5, max = 50, message = "Name must be between 5 and 50 characters")
     private String name;
 
+    @NotEmpty(message = "Email address cannot be null or empty")
+    @Email(message = "Invalid email address")
     private String email;
 
+    @Pattern(regexp = "^[0-9]{10}$", message = "Mobile number should consist of 10 digits")
     private String mobileNumber;
 
     private AccountsDto accountsDto;
--- section2/accounts/src/main/java/com/project/accounts/exception/GlobalExceptionHandler.java
+++ section2/accounts/src/main/java/com/project/accounts/exception/GlobalExceptionHandler.java
@@ -1,16 +1,35 @@
 package com.project.accounts.exception;
 
 import com.project.accounts.dto.ErrorResponseDto;
+import jakarta.validation.ConstraintViolationException;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.validation.ObjectError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 import java.time.LocalDateTime;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 @ControllerAdvice
-public class GlobalExceptionHandler {
+public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
+    @ExceptionHandler(ConstraintViolationException.class)
+    public ResponseEntity<Object> handleConstraintViolationException(ConstraintViolationException ex, WebRequest request) {
+        Map<String, String> errors = new HashMap<>();
+        ex.getConstraintViolations().forEach(constraintViolation -> {
+            String propertyPath = constraintViolation.getPropertyPath().toString();
+            errors.put(propertyPath.substring(propertyPath.indexOf(".") + 1), constraintViolation.getMessage());
+        });
+        return new ResponseEntity<>(errors, HttpStatus.BAD_REQUEST);
+    }
 
     @ExceptionHandler(CustomerAlreadyExistsException.class)
     public ResponseEntity<ErrorResponseDto> handleCustomerAlreadyExistsException(CustomerAlreadyExistsException exception, WebRequest webRequest)
@@ -22,6 +41,18 @@ public class GlobalExceptionHandler {
                 LocalDateTime.now()
         );
         return new ResponseEntity<>(errorResponseDto, HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<ErrorResponseDto> handleGlobalException(Exception exception, WebRequest webRequest)
+    {
+        ErrorResponseDto errorResponseDto = new ErrorResponseDto(
+                webRequest.getDescription(false),
+                HttpStatus.INTERNAL_SERVER_ERROR,
+                exception.getMessage(),
+                LocalDateTime.now()
+        );
+        return new ResponseEntity<>(errorResponseDto, HttpStatus.INTERNAL_SERVER_ERROR);
     }
 
     @ExceptionHandler(ResourceNotFoundException.class)
